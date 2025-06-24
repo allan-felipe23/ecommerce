49ed5cd82c3a42808ffabb3452c9a3779d6b53ca
Allan Felipe <allanborges@myyahoo.com>
2025-06-24 18:24:32 -0300

    Bean validation

--- src/main/java/com/allandev/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/allandev/ecommerce/dto/ProductDTO.java
@@ -2,12 +2,23 @@ package com.allandev.ecommerce.dto;
 
 import com.allandev.ecommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     private String name;
+
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 10, message = "Descrição precisa ter no minímo 10 caracteres")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
